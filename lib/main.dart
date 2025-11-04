3aabb3848f9c3401ba0c9cac0886dee7d6ed3197
abdelrhamanadel4-Flutter <abdelrhamanadel4@gmail.com>
2025-11-04 23:42:28 +0200

    update_main

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MoviesApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      initialRoute: AppRoute.onborading,
+      initialRoute: AppRoute.home_tab,
       routes: {
         AppRoute.home_tab: (context) => HomeTab(),
         AppRoute.loginScreen: (context) => LoginScreen(),
